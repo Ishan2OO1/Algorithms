6807d4013e50637408d75ae327859edc4b7674de
Ishan2001 <ishanchaudhary2001@gmail.com>
2021-02-27 22:01:57 +0530

    This is a c++ code to understand Insertion sorting.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,45 @@
 #include <iostream>
+
 using namespace std;
 
-void swap(int *xp, int *yp)
+void insertionSort(int arr[], int n)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
-
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n-1; i++)
-
-    // Last i elements are already in place
-    for (j = 0; j < n-i-1; j++)
-        if (arr[j] > arr[j+1])
-            swap(&arr[j], &arr[j+1]);
-}
-
-
-/* Function to print an array */
 void printArray(int arr[], int size)
 {
     int i;
-    for (i=0; i < size; i++)
+    for (i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
+
+
+
 int main()
 {
-    int arr[] = {12, 20, 34, 25, 22, 1, 90};
-    int size = sizeof(arr);
-    cout<<"unsorted array: \n";
-    printArray(arr, size);
-    bubbleSort(arr, size);
+    int arr[] = {12, 64, 34, 25, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Unsorted array: \n";
+    printArray(arr, n);
+    insertionSort(arr, n);
     cout<<"Sorted array: \n";
-    printArray(arr, size);
+    printArray(arr, n);
     return 0;
 }
 
