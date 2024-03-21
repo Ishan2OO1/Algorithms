952820faaf6efe7415e0690181a3c84b1db69daa
Ishan2001 <ishanchaudhary2001@gmail.com>
2024-03-21 18:24:39 +0530

    corrected best sort

--- OneDrive/दस्‍तावेज़/QuickSort.cpp
+++ OneDrive/दस्‍तावेज़/QuickSort.cpp
@@ -41,11 +41,10 @@ if(low<high)
 			
 
 int main() {
-int arr[]={10,7,8,9,1,5};
+int arr[]={18,1,7,2,90,5};
 int n=sizeof(arr)/sizeof(arr[0]);
 // Function call
 quickSort(arr,0,n-1);
-//Print the sorted array
 cout<<"Sorted Array\n";
 for(int i=0;i<n;i++)
 {
@@ -53,4 +52,3 @@ for(int i=0;i<n;i++)
 }
 return 0;
 }
-// This Code is Contributed By Diwakar Jha
